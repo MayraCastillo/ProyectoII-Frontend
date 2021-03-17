76504ed398c9acbd879998417f8799746c362700
MayraCastillo <mayracastillo@unicauca.edu.co>
2021-03-17 11:01:29 -0500

    Prueba

--- src/components/crear_empleado.js
+++ src/components/crear_empleado.js
@@ -23,7 +23,7 @@ export default function Employees() {
   return (
     <div className={classes.root}>
       <Typography variant="h3" component="h2" gutterBottom>
-        Crear Contrato
+        Crear
       </Typography>
      
       <Typography variant="body1" gutterBottom>
